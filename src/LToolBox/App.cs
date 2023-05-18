8826204de177fc2b367c277014825a970296cadf
LanLP <86420563+LanLP0@users.noreply.github.com>
2023-05-18 23:14:12 +0700

    Update LToolBox

---
+++ src/LToolBox/App.cs
@@ -0,0 +1,164 @@
+﻿using System.ComponentModel;
+using System.Text;
+using Common.Cli;
+using LToolBox.Tools;
+using Spectre.Console;
+using Spectre.Console.Cli;
+#if DEBUG
+using Serilog;
+#endif
+
+namespace LToolBox;
+
+internal sealed class App : Command<Settings>
+{
+    private static Tool[] _tools = null!;
+
+    private static string _prompt = null!;
+
+#if DEBUG
+    public static readonly ILogger Logger = new LoggerConfiguration()
+        .MinimumLevel.Verbose()
+        .WriteTo.Console()
+        .CreateLogger();
+#endif
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        var console = AnsiConsole.Console;
+
+        _tools = new Tool[]
+        {
+            new ComplexCalcTool(
+                console
+#if DEBUG
+                , Logger
+#endif
+            ),
+            new LCalcTool(console),
+            new MinMaxFracTool(console),
+            new PolynomialTool(console),
+            new FactTool(console),
+            new IsPrimeTool(console)
+        };
+
+        StringBuilder buffer = new();
+        buffer.Append("[Green]>[/] ");
+        var count = 1;
+        foreach (var tool in _tools)
+        {
+            buffer.Append("[Green]");
+            buffer.Append(count);
+            buffer.Append("[/]");
+            buffer.Append('-');
+            buffer.Append("[Yellow]");
+            buffer.Append(tool.ToolName);
+            buffer.Append("[/]");
+            buffer.Append(' ');
+
+            count++;
+        }
+
+        buffer.Append("\n[Green]>[/] [white]Select tool:[/] ");
+        _prompt = buffer.ToString();
+        buffer.Clear();
+
+        if (settings.ToolId.HasValue)
+        {
+            var c = settings.ToolId.Value;
+            var id = (int)c switch
+            {
+                > 48 and < 58 => c - 49,
+                > 96 and < 123 => c - 88,
+                _ => -1
+            };
+
+            if (id == -1 || id >= _tools.Length)
+                AnsiConsole.MarkupLine("[red]> Invalid tool id[/]");
+            else
+            {
+                var tool = _tools[id];
+                AnsiConsole.MarkupLine("[green]>[/] Tool: [white]{0}[/]", tool.ToolName);
+                
+                tool.Execute();
+            }
+
+            if (settings.Quit)
+                return 0;
+        }
+        
+        AnsiConsole.MarkupLine(
+                "[Green]>[/] Type [Yellow]?[/] to get help about a tool, press [Yellow]Ctrl-C[/] to exit");
+
+        if (settings.Quit)
+        {
+            PromptTool();
+            return 0;
+        }
+
+        for (;;) PromptTool();
+    }
+
+    private static void PromptTool()
+    {
+        AnsiConsole.Markup(_prompt);
+        var help = false;
+
+        for (;;)
+        {
+            var input = Console.ReadKey(true);
+
+            var id = (int)input.KeyChar switch
+            {
+                63 => -2,
+                > 48 and < 58 => input.KeyChar - 49,
+                > 96 and < 123 => input.KeyChar - 88,
+                _ => -1
+            };
+
+            switch (id)
+            {
+                case -2: // Help key
+                {
+                    AnsiConsole.Console.ClearLine();
+
+                    AnsiConsole.Markup("[Green]>[/] Help: ");
+                    help = true;
+
+                    continue;
+                }
+                case -1: // Unknown key
+                {
+                    continue;
+                }
+            }
+
+            if (id >= _tools.Length)
+                continue;
+
+            var tool = _tools[id];
+            AnsiConsole.WriteLine(tool.ToolName);
+
+            if (help)
+            {
+                tool.ExecuteHelp();
+                return;
+            }
+
+            tool.Execute();
+            return;
+        }
+    }
+}
+
+public sealed class Settings : CommandSettings
+{
+    [Description("The tool to launch after startup")]
+    [CommandOption("-t|--tool-id")]
+    public char? ToolId { get; init; }
+    
+    [Description("Quit after the first tool ran")]
+    [CommandOption("-q|--quit")]
+    [DefaultValue(false)]
+    public bool Quit { get; init; }
+}
